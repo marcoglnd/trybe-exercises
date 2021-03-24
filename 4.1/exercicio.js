48a14f333f38822daf2d917ac429fe151955c4c1
Marco Galindo <marco1408@gmail.com>
2021-03-24 18:52:49 -0300

    exercício 1 finalizado

---
+++ 4.1/exercicio.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 5;
+
+console.log('A soma de a + b é', a + b);
+console.log('A subtração de a - b é', a - b);
+console.log('A multiplicação de a * b é', a * b);
+console.log('A divisão de a / b é', a / b);
+console.log('O módulo de a % b é', a % b);
