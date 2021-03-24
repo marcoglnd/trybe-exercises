11252fc8e6468cbd08e694fa8c92d9eaa1c77a6e
Marco Galindo <marco1408@gmail.com>
2021-03-24 19:04:09 -0300

    exercício 2 completo

--- 4.1/exercicio.js
+++ 4.1/exercicio.js
@@ -1,8 +1,23 @@
-let a = 10;
+//Exercício 1
+
+// let a = 10;
+// let b = 5;
+
+// console.log('A soma de a + b é', a + b);
+// console.log('A subtração de a - b é', a - b);
+// console.log('A multiplicação de a * b é', a * b);
+// console.log('A divisão de a / b é', a / b);
+// console.log('O módulo de a % b é', a % b);
+
+//Exercício 2
+
+let a = 5;
 let b = 5;
 
-console.log('A soma de a + b é', a + b);
-console.log('A subtração de a - b é', a - b);
-console.log('A multiplicação de a * b é', a * b);
-console.log('A divisão de a / b é', a / b);
-console.log('O módulo de a % b é', a % b);
+if (a > b) {
+  console.log(a, 'é o maior número');
+} else if (a < b) {
+  console.log(b, 'é o maior número');
+} else {
+  console.log(a, 'e', b, 'são iguais');
+}
