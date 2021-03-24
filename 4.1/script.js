c14d740968609b84fca209956e2d791d216757f2
Marco Galindo <marco1408@gmail.com>
2021-03-24 18:48:34 -0300

    exercícios de fixação javascript

---
+++ 4.1/script.js
@@ -0,0 +1,66 @@
+// Exercício 1
+
+// const name = "Marco";
+// const birthCity = "Recife";
+// let birthYear = 1992;
+// console.log(name);
+// console.log(birthCity);
+// console.log(birthYear);
+// birthYear = 2020;
+// console.log(birthYear);
+// birthCity = "João Pessoa";
+// console.log(birthCity)
+
+//Exercício 2
+
+// let patientId = "50";
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof(patientId));
+// console.log(typeof(isEnrolled));
+// console.log(typeof(patientInfo));
+// console.log(typeof(patientEmail));
+// console.log(typeof(patientAge));
+
+//Exercício 3
+
+// let base = 5;
+// let altura = 8;
+// let area = base * altura;
+// let perimetro = 2*base + 2*altura;
+// console.log(area);
+// console.log(perimetro);
+
+// Exercício 4
+
+//  let nota = 60;
+//  if (nota >= 80) {
+//   console.log('Parabéns, você foi aprovada(o)!')
+// } else if (nota >= 60 && nota < 80) {
+//   console.log('Você está na nossa lista de espera')
+// } else {
+//   console.log('Você foi reprovada(o)')
+// }
+
+// Exercício 5
+
+let estadoPessoa = 'Oi';
+switch(estadoPessoa) {
+  case 'Aprovado':
+    console.log('Parabéns, você foi aprovada(o)!');
+    break;
+  case 'Lista':
+    console.log('Você está na nossa lista de espera');
+    break;
+  case 'Reprovado':
+    console.log('Você foi reprovada(o)');
+    break;
+  default:
+    console.log('Por favor insira o valor novamente');
+    break;
+}
