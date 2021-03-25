b5d0fa64404fc36e4290ac3b21b3fc5bb0a5242f
Marco Galindo <marco1408@gmail.com>
2021-03-25 13:32:35 -0300

    exercício 7 finalizado

--- 4.1/exercicio.js
+++ 4.1/exercicio.js
@@ -89,21 +89,32 @@
 
 // Exercício 7
 
-// let nota = 100
+let nota = 110
 
-// switch(nota) {
-//   case (nota >= 90):
-//     console.log('A');
-// }
+if (nota <= 100 && nota >= 90) {
+  console.log('A');
+} else if (nota < 90 && nota >= 80) {
+  console.log('B');
+} else if (nota < 80 && nota >= 70) {
+  console.log('C');
+} else if (nota < 70 && nota >= 60) {
+  console.log('D');
+} else if (nota < 60 && nota >= 50) {
+  console.log('E');
+} else if (nota < 50 && nota >= 0) {
+  console.log('F'); 
+} else {
+  console.log('Esse valor não é válido')
+}
 
 // Exercício 8
 
-let a = 3;
-let b = 5;
-let c = 7;
+// let a = 3;
+// let b = 5;
+// let c = 7;
 
-if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
-  console.log(true);
-} else {
-  console.log(false);
-}
+// if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
