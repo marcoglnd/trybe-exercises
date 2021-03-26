9f265df0fc82a57b6cfc1423539053bd256710a1
Marco Galindo <marco1408@gmail.com>
2021-03-26 09:44:57 -0300

    exercício 3 finalizado

--- 4.2/exercicio.js
+++ 4.2/exercicio.js
@@ -1,4 +1,4 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Exercício 1
 
@@ -8,8 +8,19 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Exercício 2
 
+// let soma = 0;
+// for (let index = 0; index < numbers.length ; index++) {
+//   soma = soma + numbers[index];
+// }
+// console.log(soma);
+
+// Exercício 3
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 for (let index = 0; index < numbers.length ; index++) {
   soma = soma + numbers[index];
 }
-console.log(soma);
+
+media = soma/numbers.length;
+console.log(media);
