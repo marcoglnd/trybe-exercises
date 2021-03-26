f9d0169d44135d0f85f103265bb6cdbaeb9c51bc
Marco Galindo <marco1408@gmail.com>
2021-03-26 16:15:34 -0300

    exercício 3 finalizado

--- 4.3/exercicio.js
+++ 4.3/exercicio.js
@@ -12,9 +12,18 @@
 
 // Exercício 2
 
-let n = 8;
+// let n = 8;
+// let linha = '';
+// for (let index = 0; index < n; index++) {
+//   linha = linha + '*';
+//   console.log(linha);
+// }
+
+// Exercício 3
+
+let n = 5;
 let linha = '';
-for (let index = 0; index < n; index++) {
-  linha = linha + '*';
+for (let index = 1; index <= n; index++) {
+  linha = ' '.repeat(n - index) + '*'.repeat(index);
   console.log(linha);
 }
