263400c214c60d6fcfff8cd1dc4eb77a28fd32d5
Marco Galindo <marco1408@gmail.com>
2021-03-26 11:06:30 -0300

    exercício 9 finalizado

--- 4.2/exercicio.js
+++ 4.2/exercicio.js
@@ -83,10 +83,22 @@
 
 // Exercício 8
 
+// let array = [];
+
+// for (let index = 1; index <= 25; index ++) {
+//   array.push(index);
+// }
+
+// console.log(array);
+
+// Exercício 9
+
 let array = [];
 
 for (let index = 1; index <= 25; index ++) {
   array.push(index);
 }
 
-console.log(array);
+for (let index = 0; index < array.length; index ++) {
+  console.log(array[index]/2);
+}
