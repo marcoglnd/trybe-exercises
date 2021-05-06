c23dc5cbc95b1b49454ee84e0a20fd3dad1ac8a0
Marco Galindo <marco1408@gmail.com>
2021-05-06 18:28:42 -0300

    exercício 1 e 2 finalizado

---
+++ 8.5/script.js
@@ -0,0 +1,28 @@
+// 1 - Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. O código deve retornar em sequência 2 , 15 e 54 .
+
+// const assert = require('assert');
+
+// const rectangleArea = (width, height) => width * height;
+
+// const rectangle1 = [1, 2];
+// const rectangle2 = [3, 5];
+// const rectangle3 = [6, 9];
+// const rectangles = [rectangle1, rectangle2, rectangle3];
+
+// rectangles.forEach((...number) => {
+//   assert.strictEqual(rectangleArea(), number[0] * number[1]) // altere a chamada da funcao rectangleArea
+// });
+
+// 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos. Ou seja:
+
+const assert = require('assert');
+
+const sum = (...numbers) => {
+  return numbers.reduce((acc, curr) => acc + curr, 0)
+}
+
+assert.strictEqual(sum(), 0);
+assert.strictEqual(sum(1), 1);
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(sum(1, 2, 3), 6);
+assert.strictEqual(sum(1, 2, 3, 4), 10);
