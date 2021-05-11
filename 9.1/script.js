4d3a4369944a46cfbb7352ca659c5e92383a8fdb
Marco Galindo <marco1408@gmail.com>
2021-05-11 18:44:48 -0300

    exercício 4 finalizado

--- 9.1/script.js
+++ 9.1/script.js
@@ -1,14 +1,31 @@
 // 3
 
-const getPlanet = () => {
-  const mars = {
-    name: "Mars",
-    distanceFromSun: {
-      value: 227900000,
-      measurementUnit: "kilometers",
-    },
-  };
-  setTimeout(() => console.log("Returned planet: ", mars), 4000);
+// const getPlanet = () => {
+//   const mars = {
+//     name: "Mars",
+//     distanceFromSun: {
+//       value: 227900000,
+//       measurementUnit: "kilometers",
+//     },
+//   };
+//   setTimeout(() => console.log("Returned planet: ", mars), 4000);
+// };
+
+// getPlanet(); // imprime Marte depois de 4 segundos
+
+// 4
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
 };
 
-getPlanet(); // imprime Marte depois de 4 segundos
+function sendMarsTemperature() {
+  setTimeout(() => {
+    console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`);
+  }, messageDelay())
+}
+
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
