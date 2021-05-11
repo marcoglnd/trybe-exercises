d0961855dbf6dfe15ecbb09c06bc5f3e43b54c5b
Marco Galindo <marco1408@gmail.com>
2021-05-11 18:54:04 -0300

    exercício 5 finalizado

--- 8.4/script.js
+++ 8.4/script.js
@@ -176,88 +176,153 @@
 
 // 4
 
+// const assert = require('assert');
+
+// const books = [
+//   {
+//     id: 1,
+//     name: 'As Crônicas de Gelo e Fogo',
+//     genre: 'Fantasia',
+//     author: {
+//       name: 'George R. R. Martin',
+//       birthYear: 1948,
+//     },
+//     releaseYear: 1991,
+//   },
+//   {
+//     id: 2,
+//     name: 'O Senhor dos Anéis',
+//     genre: 'Fantasia',
+//     author: {
+//       name: 'J. R. R. Tolkien',
+//       birthYear: 1892,
+//     },
+//     releaseYear: 1954,
+//   },
+//   {
+//     id: 3,
+//     name: 'Fundação',
+//     genre: 'Ficção Científica',
+//     author: {
+//       name: 'Isaac Asimov',
+//       birthYear: 1920,
+//     },
+//     releaseYear: 1951,
+//   },
+//   {
+//     id: 4,
+//     name: 'Duna',
+//     author: {
+//       name: 'Frank Herbert',
+//       birthYear: 1920,
+//     },
+//     releaseYear: 1965,
+//   },
+//   {
+//     id: 5,
+//     name: 'A Coisa',
+//     genre: 'Terror',
+//     author: {
+//       name: 'Stephen King',
+//       birthYear: 1947,
+//     },
+//     releaseYear: 1986,
+//   },
+//   {
+//     id: 6,
+//     name: 'O Chamado de Cthulhu',
+//     genre: 'Terror',
+//     author: {
+//       name: 'H. P. Lovecraft',
+//       birthYear: 1890,
+//     },
+//     releaseYear: 1928,
+//   },
+// ];
+
+// const expectedResult = {
+//   author: {
+//     birthYear: 1948,
+//     name: 'George R. R. Martin',
+//   },
+//   genre: 'Fantasia',
+//   id: 1,
+//   name: 'As Crônicas de Gelo e Fogo',
+//   releaseYear: 1991,
+// };
+
+// function longestNamedBook() {
+//   return books.reduce((acc, curr) => {
+//     if (acc.name.length > curr.name.length) {
+//       return acc;
+//     }
+//     return curr;
+//   })
+// }
+
+// assert.deepStrictEqual(longestNamedBook(), expectedResult);
+
+// 5
+
+// const assert = require('assert');
+
+// const names = [
+//   'Aanemarie', 'Adervandes', 'Akifusa',
+//   'Abegildo', 'Adicellia', 'Aladonata',
+//   'Abeladerco', 'Adieidy', 'Alarucha',
+// ];
+
+// function containsA() {
+//   return names.reduce((acc, curr) => {
+//     return acc + curr.split('').reduce((accumulator, current) => {
+//       if (current === 'a' || current === 'A') accumulator += 1;
+//       return accumulator;
+//     }, 0)
+//   }, 0)
+// }
+
+// function containsA() {
+//   const namesTogether = names.reduce((acc, curr) => {
+//     return `${acc}${curr}`;
+//   });
+//   let count = 0;
+//   for (const key in namesTogether) {
+//     if (namesTogether[key] === 'a' || namesTogether[key] === 'A') count += 1;
+//   }
+//   return count;
+// }
+
+// assert.deepStrictEqual(containsA(), 20);
+
+// 6
+
 const assert = require('assert');
 
-const books = [
-  {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: {
-      name: 'George R. R. Martin',
-      birthYear: 1948,
-    },
-    releaseYear: 1991,
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: {
-      name: 'J. R. R. Tolkien',
-      birthYear: 1892,
-    },
-    releaseYear: 1954,
-  },
-  {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Isaac Asimov',
-      birthYear: 1920,
-    },
-    releaseYear: 1951,
-  },
-  {
-    id: 4,
-    name: 'Duna',
-    author: {
-      name: 'Frank Herbert',
-      birthYear: 1920,
-    },
-    releaseYear: 1965,
-  },
-  {
-    id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
-    author: {
-      name: 'Stephen King',
-      birthYear: 1947,
-    },
-    releaseYear: 1986,
-  },
-  {
-    id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
-    author: {
-      name: 'H. P. Lovecraft',
-      birthYear: 1890,
-    },
-    releaseYear: 1928,
-  },
-];
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
-const expectedResult = {
-  author: {
-    birthYear: 1948,
-    name: 'George R. R. Martin',
-  },
-  genre: 'Fantasia',
-  id: 1,
-  name: 'As Crônicas de Gelo e Fogo',
-  releaseYear: 1991,
-};
-
-function longestNamedBook() {
-  return books.reduce((acc, curr) => {
-    if (acc.name.length > curr.name.length) {
-      return acc;
-    }
-    return curr;
-  })
+function studentAverage() {
+  const studentGrades = students.map((student, index) => ({
+    name: student,
+    average: grades[index].reduce((acc, curr) => {
+      return (acc + curr)
+    }, 0)/(grades[index].length)
+  }))
+  // const averageGrades = grades.reduce((acc, curr) => {
+  //   return acc + curr.reduce((accumulator, current) => {
+  //     return accumulator + current;
+  //   })
+  // }, []);
+  // return averageGrades.map((grades, index) => {
+  //   return grades[index]
+  // });
+  return studentGrades;
 }
 
-assert.deepStrictEqual(longestNamedBook(), expectedResult);
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+assert.deepStrictEqual(studentAverage(), expected);
--- 9.1/script.js
+++ 9.1/script.js
@@ -15,6 +15,23 @@
 
 // 4
 
+// const messageDelay = () => Math.floor(Math.random() * 5000);
+
+// const getMarsTemperature = () => {
+//   const maxTemperature = 58;
+//   return Math.floor(Math.random() * maxTemperature);
+// };
+
+// function sendMarsTemperature() {
+//   setTimeout(() => {
+//     console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`);
+//   }, messageDelay())
+// }
+
+// sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+// 5
+
 const messageDelay = () => Math.floor(Math.random() * 5000);
 
 const getMarsTemperature = () => {
@@ -22,10 +39,18 @@ const getMarsTemperature = () => {
   return Math.floor(Math.random() * maxTemperature);
 };
 
-function sendMarsTemperature() {
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+function sendMarsTemperature(callback) {
   setTimeout(() => {
-    console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`);
+    const temperature = getMarsTemperature();
+    console.log(`Mars temperature is: ${temperature} degree Celsius`);
+    callback(temperature);
   }, messageDelay())
 }
 
-sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
