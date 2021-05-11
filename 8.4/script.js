17c13378278cc36c979f917101ecde00d861a31a
Marco Galindo <marco1408@gmail.com>
2021-05-11 13:12:25 -0300

    requisito 4 finalizado

--- 8.4/script.js
+++ 8.4/script.js
@@ -95,6 +95,87 @@
 
 // 3
 
+// const assert = require('assert');
+
+// const books = [
+//   {
+//     id: 1,
+//     name: 'As Crônicas de Gelo e Fogo',
+//     genre: 'Fantasia',
+//     author: {
+//       name: 'George R. R. Martin',
+//       birthYear: 1948,
+//     },
+//     releaseYear: 1991,
+//   },
+//   {
+//     id: 2,
+//     name: 'O Senhor dos Anéis',
+//     genre: 'Fantasia',
+//     author: {
+//       name: 'J. R. R. Tolkien',
+//       birthYear: 1892,
+//     },
+//     releaseYear: 1954,
+//   },
+//   {
+//     id: 3,
+//     name: 'Fundação',
+//     genre: 'Ficção Científica',
+//     author: {
+//       name: 'Isaac Asimov',
+//       birthYear: 1920,
+//     },
+//     releaseYear: 1951,
+//   },
+//   {
+//     id: 4,
+//     name: 'Duna',
+//     genre: 'Ficção Científica',
+//     author: {
+//       name: 'Frank Herbert',
+//       birthYear: 1920,
+//     },
+//     releaseYear: 1965,
+//   },
+//   {
+//     id: 5,
+//     name: 'A Coisa',
+//     genre: 'Terror',
+//     author: {
+//       name: 'Stephen King',
+//       birthYear: 1947,
+//     },
+//     releaseYear: 1986,
+//   },
+//   {
+//     id: 6,
+//     name: 'O Chamado de Cthulhu',
+//     genre: 'Terror',
+//     author: {
+//       name: 'H. P. Lovecraft',
+//       birthYear: 1890,
+//     },
+//     releaseYear: 1928,
+//   },
+// ];
+
+// const expectedResult = 43;
+
+// function averageAge() {
+//   const allReleaseYears = books.reduce((acc, curr) => {
+//     return acc + curr.releaseYear;
+//   }, 0);
+//   const allBirthYears = books.reduce((acc, curr) => {
+//     return acc + curr.author.birthYear;
+//   }, 0);
+//   return (allReleaseYears - allBirthYears)/books.length;
+// }
+
+// assert.strictEqual(averageAge(), expectedResult);
+
+// 4
+
 const assert = require('assert');
 
 const books = [
@@ -131,7 +212,6 @@ const books = [
   {
     id: 4,
     name: 'Duna',
-    genre: 'Ficção Científica',
     author: {
       name: 'Frank Herbert',
       birthYear: 1920,
@@ -160,16 +240,24 @@ const books = [
   },
 ];
 
-const expectedResult = 43;
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
 
-function averageAge() {
-  const allReleaseYears = books.reduce((acc, curr) => {
-    return acc + curr.releaseYear;
-  }, 0);
-  const allBirthYears = books.reduce((acc, curr) => {
-    return acc + curr.author.birthYear;
-  }, 0);
-  return (allReleaseYears - allBirthYears)/books.length;
+function longestNamedBook() {
+  return books.reduce((acc, curr) => {
+    if (acc.name.length > curr.name.length) {
+      return acc;
+    }
+    return curr;
+  })
 }
 
-assert.strictEqual(averageAge(), expectedResult);
+assert.deepStrictEqual(longestNamedBook(), expectedResult);
