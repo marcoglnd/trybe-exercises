fdb9e68150d36b81341dc3ceac2829ad24b11a11
Marco Galindo <marco1408@gmail.com>
2021-05-19 20:03:58 -0300

    exercício 3 finalizado

--- 10.2/script.test.js
+++ 10.2/script.test.js
@@ -1,4 +1,5 @@
 const { expect, describe, it } = require('@jest/globals')
+const { async } = require('rsvp')
 // const { describe } = require('yargs')
 // const uppercase = require('./script')
 
@@ -11,24 +12,44 @@ const { expect, describe, it } = require('@jest/globals')
 
 const getUserName = require('./script')
 
+// describe('quando o usuário existe', () => {
+//   it('retorne o usuário Mark', () => {
+//     return getUserName(4).then(user => {
+//       expect(user).toEqual('Mark');
+//     })
+//   })
+//   it('retorne o usuário Paul', () => {
+//     return getUserName(5).then(user => {
+//       expect(user).toEqual('Paul');
+//     })
+//   })
+// })
+
+// describe('quando o usuário não existe', () => {
+//   it('retorna mensagem de erro', () => {
+//     return getUserName(1).catch(error => {
+//       expect(error).toEqual({ error: 'User with 1 not found.' })
+//     })
+//   })
+// })
+
 describe('quando o usuário existe', () => {
-  it('retorne o usuário Mark', () => {
-    return getUserName(4).then(user => {
-      expect(user).toEqual('Mark');
-    })
+  it('retorne o usuário Mark', async () => {
+    const user = await getUserName(4);
+    expect(user).toEqual('Mark');
   })
-  it('retorne o usuário Paul', () => {
-    return getUserName(5).then(user => {
-      expect(user).toEqual('Paul');
-    })
+  it('retorne o usuário Paul', async () => {
+    const user = await getUserName(5);
+    expect(user).toEqual('Paul');
   })
 })
 
 describe('quando o usuário não existe', () => {
-  it('retorna mensagem de erro', () => {
-    return getUserName(1).catch(error => {
+  it('retorna mensagem de erro', async () => {
+    try {
+      await getUserName(1);
+    } catch(error) {
       expect(error).toEqual({ error: 'User with 1 not found.' })
-    })
+    }
   })
 })
-
